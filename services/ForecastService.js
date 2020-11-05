a4eaaa0b3accaa4e4fd7c5a9b31f7d84d7142184
Oscar Pou <opou@apexsystems.com>
2020-11-05 11:32:51 -0600

    Add ForecastService to consume the local Forecast API.

---
+++ services/ForecastService.js
@@ -0,0 +1,24 @@
+import buildUrl from 'build-url';
+import fetch from 'node-fetch';
+const BASE_URL = 'http://localhost:3000/';
+
+export default async function getForecast(query) {
+
+    const url = buildUrl(BASE_URL, {
+        path: '/api/forecast',
+        queryParams: query
+    });
+    try {
+        let response = await fetch(url);
+        if (response.status < 200 || response.status >= 300) {
+            let message = `Error fetching api URL '${url}'\n`;
+            message += `(${response.status}) : ${response.statusText}`;
+            throw Error(message);
+        }
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+    
+}
